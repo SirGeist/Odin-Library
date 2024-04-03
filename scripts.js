9ae2d7400263fe31b71799a73c3a81417a7c324a
SirGeist <trinh1234533@yahoo.com>
2024-04-02 22:19:44 -0500

    added functionality to remove and toggle books, completed the book-form so that users can input info into form that will then be stored within an array

--- scripts.js
+++ scripts.js
@@ -7,12 +7,16 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary(book) {}
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
 
 function displayBooks() {
   const libraryDiv = document.getElementById("library");
-  bookDiv.classList.innerHTML = "";
+  libraryDiv.innerHTML = "";
 
+  // Looping through the library array while assigning each
+  // component of the book
   myLibrary.forEach((book, index) => {
     const bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
@@ -29,11 +33,67 @@ function displayBooks() {
     const readPara = document.createElement("p");
     readPara.textContent = `Read: ${book.read ? "Yes" : "No"}`;
 
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+
+    // Redisplay the current selection of books available after
+    // removal
+    removeButton.addEventListener("click", () => {
+      removeBook(index);
+      displayBooks();
+    });
+
+    // Redisplaying the read status after click has been detected
+    const toggleReadButton = document.createElement("button");
+    toggleReadButton.textContent = "Toggle Read";
+    toggleReadButton.addEventListener("click", () => {
+      toggleReadStatus(index);
+      displayBooks();
+    });
+
+    // Appending each component of the book
     bookDiv.appendChild(titlePara);
     bookDiv.appendChild(authorPara);
     bookDiv.appendChild(pagesPara);
     bookDiv.appendChild(readPara);
+    bookDiv.appendChild(removeButton);
+    bookDiv.appendChild(toggleReadButton);
 
+    // Finally appending the book to the library
     libraryDiv.appendChild(bookDiv);
   });
 }
+
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+}
+
+function toggleReadStatus(index) {
+  myLibrary[index].read = !myLibrary[index].read;
+}
+
+// Every time we create a new book, we style it in block format
+document.getElementById("new-book-btn").addEventListener("click", () => {
+  const formDiv = document.getElementById("new-book-form");
+  formDiv.style.display = "block";
+});
+
+// When we input information into our forms
+document.getElementById("book-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").checked;
+
+  // Creating a new instance called newBook with the properties passed on
+  const newBook = new Book(title, author, pages, read);
+  addBookToLibrary(newBook);
+
+  // Reseting the form fields after submitting
+  document.getElementById("book-form").reset();
+  document.getElementById("new-book-form").style.display = "none";
+
+  displayBooks();
+});
